0b1fbd43073991dae498e8bcab74f715d369aaf7
danielalfb <danielalfbarbosa@gmail.com>
2022-09-29 22:03:48 -0300

    add axios and create service

---
+++ packages/frontend/src/components/ServiceCards/SoldServiceCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../Header';
+import { Box } from '@mui/material';
+
+const SoldServiceCard =({children}) =>{
+   
+}
+
+export default SoldServiceCard;
